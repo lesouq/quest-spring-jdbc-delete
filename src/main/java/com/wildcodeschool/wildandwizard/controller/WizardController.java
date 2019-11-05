a922cd0bc48a4ead2d0d9c5134edb34d8d3ba3e2
Adrien Souquere <adriensouquere@gmail.com>
2019-11-05 16:32:51 +0100

    terminé

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -1,6 +1,12 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.repository.WizardRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WizardController {
 
-    private WizardRepository repository = new WizardRepository();
+    private static final String DB_URL = null;
+	private static final String DB_PASSWORD = null;
+	private static final String DB_USER = null;
+	private WizardRepository repository = new WizardRepository();
 
     @GetMapping("/wizard/delete")
     public String deleteWizard(@RequestParam Long id) {
@@ -26,4 +35,23 @@ public class WizardController {
 
         return "wizard_get_all";
     }
+    
+    public void deleteById(Long id) {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM wizard WHERE id=?"
+            );
+            statement.setLong(1, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("failed to delete data");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
